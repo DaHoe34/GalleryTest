b461d4a584d7a0233f91c8f9a917401590156be6
Dahoe Shin <kity990304@gmail.com>
2016-10-25 20:17:34 +0900

    #3 Touch image

--- app/src/main/java/kr/hs/emirim/dahoe0304/gallerytest/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/dahoe0304/gallerytest/MainActivity.java
@@ -1,13 +1,129 @@
 package kr.hs.emirim.dahoe0304.gallerytest;
 
+import android.content.Context;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Gallery;
+import android.widget.ImageView;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 public class MainActivity extends AppCompatActivity {
+
+    Gallery gallery;
+    ImageView imgMain;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        gallery = (Gallery) findViewById(R.id.gallery_1);
+        imgMain = (ImageView) findViewById(R.id.imgv_main);
+        MyGalleryAdapter adapter = new MyGalleryAdapter(this);
+        gallery.setAdapter(adapter);
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://kr.hs.emirim.dahoe0304.gallerytest/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://kr.hs.emirim.dahoe0304.gallerytest/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
+
+    public class MyGalleryAdapter extends BaseAdapter {
+
+        Context context;
+        int[] posterIDs = {R.drawable.line1, R.drawable.line2, R.drawable.line3,
+                R.drawable.line4, R.drawable.line5, R.drawable.line6,
+                R.drawable.line7, R.drawable.line8, R.drawable.line9,
+                R.drawable.line10};
+
+        public MyGalleryAdapter(Context context) {   //어댑터가 사용되는 문맥
+            this.context = context;
+        }
+
+        @Override
+        public int getCount() {
+            return posterIDs.length;    //항목의 갯수
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ImageView imgV = new ImageView(context);
+            imgV.setLayoutParams(new Gallery.LayoutParams(100, 150));
+            imgV.setScaleType(ImageView.ScaleType.FIT_CENTER);  //가운데에 맞춰서 보여준다.
+            imgV.setPadding(5, 5, 5, 5);
+            imgV.setImageResource(posterIDs[position]);
+            final int pos = position;
+            imgV.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+                    imgMain.setScaleType(ImageView.ScaleType.FIT_CENTER);
+                    imgMain.setImageResource(posterIDs[pos]);
+                    return false;
+                }
+            });
+            return imgV;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
     }
 }
